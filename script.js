3a563fff20182e51c8f4def25c62c55e62fb0bf5
Umesh Gurung <umesh.grg2058@gmai.com>
2023-05-04 23:21:08 +0545

    responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav_link = document.querySelector('.nav-links');
+
+    burger.addEventListener("click", () => {
+        nav_link.classList.toggle('nav-active');
+    });
+}
+
+
+navSlide()
